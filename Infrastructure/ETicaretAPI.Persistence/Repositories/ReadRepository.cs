2d0982c3249a3798b432e1b1ac0a5ff535a81c75
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-12-04 15:42:45 +0300

    Generic concrete classes added

---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,46 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities.Common;
+using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : BaseEntity
+    {
+        private readonly ETicaretAPIDbContext context;
+
+        public ReadRepository(ETicaretAPIDbContext context)
+        {
+            this.context = context;
+        }
+
+        public DbSet<T> Table => context.Set<T>();
+
+        public IQueryable<T> GetAll()
+        {
+            return Table;
+        }
+
+        public async Task<T> GetByIdAsync(string id)
+        {
+            //Marker Pattern - firstordefaultasync metodunda string id'yi bulmak için baseEntity'den gelen (x.ID) ile yakaladık
+            return await Table.FirstOrDefaultAsync(x => x.ID == Guid.Parse(id));
+        }
+
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> expression)
+        {
+            return await Table.FirstOrDefaultAsync(expression);
+        }
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> expression)
+        {
+            return Table.Where(expression);
+        }
+    }
+}
