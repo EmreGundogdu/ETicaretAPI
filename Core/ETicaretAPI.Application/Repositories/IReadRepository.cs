379679eb30627bcb5a3979abdba1a477882870b5
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-12-04 15:10:32 +0300

    IReadRepository added

--- Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
+++ Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,9 @@ namespace ETicaretAPI.Application.Repositories
 {
     public interface IReadRepository<T> : IRepository<T> where T : class
     {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetWhere(Expression<Func<T,bool>> expression);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> expression);
+        Task<T> GetByIdAsync(string id);
     }
 }
