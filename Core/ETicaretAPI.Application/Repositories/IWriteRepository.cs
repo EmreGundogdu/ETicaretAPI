e3eb8365cc5c65af838a1b0b9913d1181fad4eba
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-12-04 15:10:45 +0300

    IWriteRepository added

--- Core/ETicaretAPI.Application/Repositories/IWriteRepository.cs
+++ Core/ETicaretAPI.Application/Repositories/IWriteRepository.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace ETicaretAPI.Application.Repositories
 {
-    public interface IWriteRepository
+    public interface IWriteRepository<T> : IRepository<T> where T : class
     {
+        Task<bool> AddAsync(T model);
+        Task<bool> AddAsync(List<T> model);
+        Task<bool> RemoveAsync(T model);
+        Task<bool> RemoveAsync(string id);
+        Task<bool> UpdateAsync(T model);
     }
 }
