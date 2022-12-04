1a6d8e0fef18863b8cccea66041b4f649668440c
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-12-04 15:42:28 +0300

    Product concrete classes added

---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/Product/ProductReadRepository.cs
@@ -0,0 +1,13 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
+using ETicaretAPI.Persistence.Contexts;
+
+namespace ETicaretAPI.Persistence.Repositories
+{
+    public class ProductReadRepository : ReadRepository<Product>, IProductReadRepository
+    {
+        public ProductReadRepository(ETicaretAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
