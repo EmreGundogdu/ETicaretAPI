011d52886d023e7e151d417791ba8d2f0f8b527e
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-12-12 12:42:20 +0300

    Cors policy configured

--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddCors(opt =>
 {
     opt.AddDefaultPolicy(policy =>
     {
-        policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+        policy.WithOrigins("http://localhost:4200","https://localhost:4200").AllowAnyHeader().AllowAnyMethod();
     });
 });
 builder.Services.AddControllers();
