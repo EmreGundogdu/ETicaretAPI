98596795236076ecbc94c691dcef558cbc1a93db
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-12-18 02:57:53 +0300

    File nampeOperation added

--- Core/ETicaretAPI.Application/Services/IFileService.cs
+++ Core/ETicaretAPI.Application/Services/IFileService.cs
@@ -9,6 +9,6 @@ namespace ETicaretAPI.Application.Services;
 public interface IFileService
 {
     Task<List<(string fileName,string path)>> UploadAsync(string path,IFormFileCollection files);
-    Task<string> FileRenameAsync(string fileName);
+    Task<string> FileRenameAsync(string path,string fileName, bool first);
     Task<bool> CopyFileAsync(string path,IFormFile formFile);
 }
