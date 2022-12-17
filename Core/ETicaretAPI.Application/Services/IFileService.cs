507bc898819c673789ad4add92b75efdefccf713
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-12-18 01:18:57 +0300

    IFileService and method signature added

---
+++ Core/ETicaretAPI.Application/Services/IFileService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Services;
+public interface IFileService
+{
+    Task UploadAsync(string path,IFormFileCollection files);
+}
