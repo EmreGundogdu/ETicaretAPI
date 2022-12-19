aee4609f1a0056f95438fcb8d8f3821ae40c90ba
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-12-20 00:55:04 +0300

    remove product image commands added

---
+++ Core/ETicaretAPI.Application/Features/Commands/ProductImage/RemoveProductImage/RemoveProductImageCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using ETicaretAPI.Application.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using P = ETicaretAPI.Domain.Entities;
+namespace ETicaretAPI.Application.Features.Commands.ProductImage.RemoveProductImage
+{
+    public class RemoveProductImageCommandHandler : IRequestHandler<RemoveProductImageCommandRequest, RemoveProductImageCommandResponse>
+    {
+        readonly IProductReadRepository productReadRepository;
+        readonly IProductWriteRepository productWriteRepository;
+
+        public RemoveProductImageCommandHandler(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
+        {
+            this.productReadRepository = productReadRepository;
+            this.productWriteRepository = productWriteRepository;
+        }
+
+        public async Task<RemoveProductImageCommandResponse> Handle(RemoveProductImageCommandRequest request, CancellationToken cancellationToken)
+        {
+            P::Product? product = await productReadRepository.Table.Include(x => x.ProductImageFiles).FirstOrDefaultAsync(x => x.ID == Guid.Parse(request.Id));
+            var productImageFile = product?.ProductImageFiles.FirstOrDefault(x => x.ID == Guid.Parse(request.ImageId));
+            if (productImageFile != null)
+                product?.ProductImageFiles.Remove(productImageFile);
+            await productWriteRepository.SaveAsync();
+            return new();
+        }
+    }
+}
