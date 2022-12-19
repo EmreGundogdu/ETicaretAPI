38ac84bf02cbf3ed76272926781946bf3034bb6b
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-12-20 00:54:47 +0300

    upload product commands added

--- Core/ETicaretAPI.Application/Features/Commands/Product/UpdateProduct/UpdateProductCommandHandler.cs
+++ Core/ETicaretAPI.Application/Features/Commands/Product/UpdateProduct/UpdateProductCommandHandler.cs
@@ -1,17 +1,29 @@
-﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ETicaretAPI.Application.Repositories;
+using MediatR;
 
 namespace ETicaretAPI.Application.Features.Commands.Product.UpdateProduct
 {
     public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommandRequest, UpdateProductCommandResponse>
     {
+        readonly IProductReadRepository productReadRepository;
+        readonly IProductWriteRepository productWriteRepository;
+        public UpdateProductCommandHandler(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
+        {
+            this.productReadRepository = productReadRepository;
+            this.productWriteRepository = productWriteRepository;
+        }
+
         public async Task<UpdateProductCommandResponse> Handle(UpdateProductCommandRequest request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var product = await productReadRepository.GetByIdAsync(request.Id);
+            product.Name = request.Name;
+            product.Stock = request.Stock;
+            product.Price = request.Price;
+            await productWriteRepository.SaveAsync();
+            return new()
+            {
+
+            };
         }
     }
 }
