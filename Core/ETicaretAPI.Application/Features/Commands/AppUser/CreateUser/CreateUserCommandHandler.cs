16c19a0366c2ca218f0193748749a00db565647e
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-12-25 11:28:46 +0300

    Create user commands added and the method added to user controller

---
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using ETicaretAPI.Application.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using U = ETicaretAPI.Domain.Entities.Identity;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.CreateUser
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommandRequest, CreateUserCommandResponse>
+    {
+        readonly UserManager<U.AppUser> userManager;
+
+        public CreateUserCommandHandler(UserManager<U.AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<CreateUserCommandResponse> Handle(CreateUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            IdentityResult result = await userManager.CreateAsync(new()
+            {
+                UserName = request.Username,
+                Email = request.Email,
+                NameSurname = request.NameSurname
+            }, request.Password);
+            CreateUserCommandResponse response = new() { Succeeded = result.Succeeded };
+            if (result.Succeeded)
+                response.Message = "Kullanıcı Kaydı Başarıyla Tamamlandı";
+            else
+                foreach (var item in result.Errors)
+                {
+                    response.Message += $"{item.Code} - {item.Description}<br>";
+                }
+            return response;
+        }
+    }
+}
