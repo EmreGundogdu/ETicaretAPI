d04ee40ef5f95997d56ff189b163369143fcb483
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-05 14:45:05 +0300

    GoogleLoginCommandHandler and ITokenHandler updated

--- Infrastructure/ETicaretAPI.Infrastructure/Services/Token/TokenHandler.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/Token/TokenHandler.cs
@@ -15,14 +15,14 @@ namespace ETicaretAPI.Infrastructure.Services.Token
             this.configuration = configuration;
         }
 
-        public Application.DTOs.Token CreateAccessToken(int minute)
+        public Application.DTOs.Token CreateAccessToken(int second)
         {
             Application.DTOs.Token token = new();
             SymmetricSecurityKey symmetricSecurityKey = new(Encoding.UTF8.GetBytes(configuration["Token:SecurityKey"]));
 
             SigningCredentials signingCredentials = new(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
 
-            token.Expiration = DateTime.UtcNow.AddMinutes(minute);
+            token.Expiration = DateTime.UtcNow.AddMinutes(second);
             JwtSecurityToken securityToken = new(
                 audience: configuration["Token:Audience"],
                 issuer: configuration["Token:Issuer"],
