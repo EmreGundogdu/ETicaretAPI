fa9b1d194ee188477d2d9a45242737402fbb580c
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-06 12:47:25 +0300

    SignalR constant message and ProductHubService added

--- Infrastructure/ETicaretAPI.SignalR/HubServices/ProductHubService.cs
+++ Infrastructure/ETicaretAPI.SignalR/HubServices/ProductHubService.cs
@@ -1,13 +1,21 @@
 ﻿using ETicaretAPI.Application.Abstractions.Hubs;
+using ETicaretAPI.SignalR.Hubs;
+using Microsoft.AspNetCore.SignalR;
 
 namespace ETicaretAPI.SignalR.HubServices
 {
     public class ProductHubService : IProductHubService
     {
+        readonly IHubContext<ProductHub> hubContext;
 
-        public Task ProductAddedMessageAsync(string message)
+        public ProductHubService(IHubContext<ProductHub> hubContext)
         {
-            throw new NotImplementedException();
+            this.hubContext = hubContext;
+        }
+
+        public async Task ProductAddedMessageAsync(string message)
+        {
+            await hubContext.Clients.All.SendAsync(ReceiveFunctionNames.ProductAddedMessage, message);
         }
     }
 }
