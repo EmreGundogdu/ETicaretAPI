e256c1858007d2734ec8d4157b284e75e1d9ed02
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-07 18:16:28 +0300

    Basket, basket item and the other entities relations added between those entities

---
+++ Core/ETicaretAPI.Domain/Entities/Basket.cs
@@ -0,0 +1,12 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using ETicaretAPI.Domain.Entities.Identity;
+
+namespace ETicaretAPI.Domain.Entities
+{
+    public class Basket : BaseEntity
+    {
+        public string UserId { get; set; }
+        public AppUser User { get; set; }
+        public ICollection<BasketItem> BasketItems { get; set; }
+    }
+}
---
+++ Core/ETicaretAPI.Domain/Entities/BasketItem.cs
@@ -0,0 +1,13 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+
+namespace ETicaretAPI.Domain.Entities
+{
+    public class BasketItem : BaseEntity
+    {
+        public Guid BasketId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public Basket Basket { get; set; }
+        public Product Product { get; set; }
+    }
+}
