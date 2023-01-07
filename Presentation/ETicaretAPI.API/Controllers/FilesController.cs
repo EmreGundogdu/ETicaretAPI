81d0646b6e981b1a08e8e652f878c839097a97c0
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-07 17:45:38 +0300

    GetBaseUrl method and files controller added

---
+++ Presentation/ETicaretAPI.API/Controllers/FilesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        readonly IConfiguration configuration;
+
+        public FilesController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetBaseUrl()
+        {
+            return Ok(configuration["BaseStorageUrl"]);
+        }
+    }
+}
