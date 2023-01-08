e01a9ba475ed961467db8f76d6323ecb71f419fd
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-08 16:17:54 +0300

    RemoveBasketItems commands added

---
+++ Core/ETicaretAPI.Application/Features/Commands/Basket/RemoveBasketItem/RemoveBasketItemCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.Basket.RemoveBasketItem
+{
+    public class RemoveBasketItemCommandHandler : IRequestHandler<RemoveBasketItemCommandRequest, RemoveBasketItemCommandResponse>
+    {
+        readonly IBasketService basketService;
+
+        public RemoveBasketItemCommandHandler(IBasketService basketService)
+        {
+            this.basketService = basketService;
+        }
+
+        public async Task<RemoveBasketItemCommandResponse> Handle(RemoveBasketItemCommandRequest request, CancellationToken cancellationToken)
+        {
+            await basketService.RemoveBasketItemAsync(request.BasketItemId);
+            return new();
+        }
+    }
+}
