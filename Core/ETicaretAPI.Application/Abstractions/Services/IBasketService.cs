b2065ff926bd0f29b9c8692aa7f8bb0f33468f12
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-08 13:56:49 +0300

    IBasketService interface added

---
+++ Core/ETicaretAPI.Application/Abstractions/Services/IBasketService.cs
@@ -0,0 +1,19 @@
+﻿using ETicaretAPI.Application.ViewModels.Baskets;
+using ETicaretAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Abstractions.Services
+{
+    public interface IBasketService
+    {
+        Task<List<BasketItem>> GetBasketItems();
+        Task AddItemToBasketAsync(VM_Create_BasketItem vM_Create_BasketItem);
+        Task UpdateQuantityAsync(VM_Update_BasketItem vM_Update_BasketItem);
+        Task RemoveBasketItemAsync(string basketItemId);
+    }
+}
