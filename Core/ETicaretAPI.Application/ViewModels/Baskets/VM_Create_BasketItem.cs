c05e817be7c7c148c49ace9f0a62682365b19497
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-08 14:34:40 +0300

    Viewmodels updated

--- Core/ETicaretAPI.Application/ViewModels/Baskets/VM_Create_BasketItem.cs
+++ Core/ETicaretAPI.Application/ViewModels/Baskets/VM_Create_BasketItem.cs
@@ -8,5 +8,7 @@ namespace ETicaretAPI.Application.ViewModels.Baskets
 {
     public class VM_Create_BasketItem
     {
+        public string ProductId { get; set; }
+        public int Quantity { get; set; }
     }
 }
--- Core/ETicaretAPI.Application/ViewModels/Baskets/VM_Update_BasketItem.cs
+++ Core/ETicaretAPI.Application/ViewModels/Baskets/VM_Update_BasketItem.cs
@@ -8,5 +8,7 @@ namespace ETicaretAPI.Application.ViewModels.Baskets
 {
     public class VM_Update_BasketItem
     {
+        public string BasketItemId { get; set; }
+        public int Quantity { get; set; }
     }
 }
