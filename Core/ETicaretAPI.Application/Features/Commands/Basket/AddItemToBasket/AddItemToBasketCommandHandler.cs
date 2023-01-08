87b6a0adf699d0b725577a228c2abdf4368f86f3
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-08 16:17:37 +0300

    AddItemToBasket commands added

---
+++ Core/ETicaretAPI.Application/Features/Commands/Basket/AddItemToBasket/AddItemToBasketCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.Basket.AddItemToBasket
+{
+    public class AddItemToBasketCommandHandler : IRequestHandler<AddItemToBasketCommandRequest, AddItemToBasketCommandResponse>
+    {
+        readonly IBasketService basketService;
+
+        public AddItemToBasketCommandHandler(IBasketService basketService)
+        {
+            this.basketService = basketService;
+        }
+
+        public async Task<AddItemToBasketCommandResponse> Handle(AddItemToBasketCommandRequest request, CancellationToken cancellationToken)
+        {
+            await basketService.AddItemToBasketAsync(new()
+            {
+                ProductId = request.ProductId,
+                Quantity = request.Quantity
+            });
+            return new();
+        }
+    }
+}
