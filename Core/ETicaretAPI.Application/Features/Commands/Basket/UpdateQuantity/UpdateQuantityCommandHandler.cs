084275f284362e1e361caaddc84a2215e4631fec
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-08 16:18:11 +0300

    UpdateQuantity commands added

---
+++ Core/ETicaretAPI.Application/Features/Commands/Basket/UpdateQuantity/UpdateQuantityCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.Basket.UpdateQuantity
+{
+    public class UpdateQuantityCommandHandler : IRequestHandler<UpdateQuantityCommandRequest, UpdateQuantityCommandResponse>
+    {
+        readonly IBasketService basketService;
+
+        public UpdateQuantityCommandHandler(IBasketService basketService)
+        {
+            this.basketService = basketService;
+        }
+
+        public async Task<UpdateQuantityCommandResponse> Handle(UpdateQuantityCommandRequest request, CancellationToken cancellationToken)
+        {
+            await basketService.UpdateQuantityAsync(new()
+            {
+                BasketItemId = request.BasketItemId,
+                Quantity = request.Quantity
+            });
+            return new();
+        }
+    }
+}
