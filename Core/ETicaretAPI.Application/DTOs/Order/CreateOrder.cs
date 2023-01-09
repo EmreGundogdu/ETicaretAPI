79a15c86330b755fc0560bc7001e735997ae0998
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-09 22:10:00 +0300

    Entity and dto updated of order

--- Core/ETicaretAPI.Application/DTOs/Order/CreateOrder.cs
+++ Core/ETicaretAPI.Application/DTOs/Order/CreateOrder.cs
@@ -2,7 +2,7 @@
 {
     public class CreateOrder
     {
-        public string BasketId { get; set; }
+        public string? BasketId { get; set; }
         public string Description { get; set; }
         public string Address { get; set; }
     }
