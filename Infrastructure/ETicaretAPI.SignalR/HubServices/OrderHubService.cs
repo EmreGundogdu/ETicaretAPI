bec80e2fbdfb043b9d61d316bc58fd5249d062f1
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-09 14:46:53 +0300

    Order SignalR operations added

---
+++ Infrastructure/ETicaretAPI.SignalR/HubServices/OrderHubService.cs
@@ -0,0 +1,19 @@
+﻿using ETicaretAPI.Application.Abstractions.Hubs;
+using ETicaretAPI.SignalR.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ETicaretAPI.SignalR.HubServices
+{
+    public class OrderHubService : IOrderHubService
+    {
+        readonly IHubContext<OrderHub> hubContext;
+
+        public OrderHubService(IHubContext<OrderHub> hubContext)
+        {
+            this.hubContext = hubContext;
+        }
+
+        public async Task OrderAddedMessageAsync(string message)
+           => await hubContext.Clients.All.SendAsync(ReceiveFunctionNames.OrderAddedMessage, message);
+    }
+}
--- Infrastructure/ETicaretAPI.SignalR/ServiceRegistration.cs
+++ Infrastructure/ETicaretAPI.SignalR/ServiceRegistration.cs
@@ -9,6 +9,7 @@ namespace ETicaretAPI.SignalR
         public static void AddSignalRServices(this IServiceCollection services)
         {
             services.AddTransient<IProductHubService, ProductHubService>();
+            services.AddTransient<IOrderHubService, OrderHubService>();
             services.AddSignalR();
         }
     }
