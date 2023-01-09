7922bd563ab214daf9640a63b41551a165e13fba
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-09 14:47:39 +0300

    IOrderService interface and the concrete class added

--- Core/ETicaretAPI.Application/Abstractions/Services/IBasketService.cs
+++ Core/ETicaretAPI.Application/Abstractions/Services/IBasketService.cs
@@ -1,11 +1,5 @@
 ﻿using ETicaretAPI.Application.ViewModels.Baskets;
 using ETicaretAPI.Domain.Entities;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ETicaretAPI.Application.Abstractions.Services
 {
@@ -15,5 +9,6 @@ namespace ETicaretAPI.Application.Abstractions.Services
         Task AddItemToBasketAsync(VM_Create_BasketItem vM_Create_BasketItem);
         Task UpdateQuantityAsync(VM_Update_BasketItem vM_Update_BasketItem);
         Task RemoveBasketItemAsync(string basketItemId);
+
     }
 }
---
+++ Infrastructure/ETicaretAPI.Persistence/Services/OrderService.cs
@@ -0,0 +1,32 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using ETicaretAPI.Application.DTOs.Order;
+using ETicaretAPI.Application.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Services
+{
+    public class OrderService : IOrderService
+    {
+        readonly IOrderWriteRepository orderWriteRepository;
+
+        public OrderService(IOrderWriteRepository orderWriteRepository)
+        {
+            this.orderWriteRepository = orderWriteRepository;
+        }
+
+        public async Task CreateOrder(CreateOrder createOrder)
+        {
+            await orderWriteRepository.AddAsync(new()
+            {
+                Address = createOrder.Address,
+                Description = createOrder.Description,
+                BasketId = Guid.Parse(createOrder.BasketId)
+            });
+            await orderWriteRepository.SaveAsync();
+        }
+    }
+}
