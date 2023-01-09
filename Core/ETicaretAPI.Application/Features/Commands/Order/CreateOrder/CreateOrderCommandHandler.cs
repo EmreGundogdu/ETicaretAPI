fd50164653d976cdee42d5bca1474d0f4c450650
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-09 14:47:52 +0300

    CreateOrder Commands added

---
+++ Core/ETicaretAPI.Application/Features/Commands/Order/CreateOrder/CreateOrderCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.Order.CreateOrder
+{
+    public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommandRequest, CreateOrderCommandResponse>
+    {
+        readonly IOrderService orderService;
+
+        public CreateOrderCommandHandler(IOrderService orderService)
+        {
+            this.orderService = orderService;
+        }
+
+        public async Task<CreateOrderCommandResponse> Handle(CreateOrderCommandRequest request, CancellationToken cancellationToken)
+        {
+            await orderService.CreateOrder(new()
+            {
+                Address = request.Address,
+                Description = request.Description
+            });
+        }
+    }
+}
