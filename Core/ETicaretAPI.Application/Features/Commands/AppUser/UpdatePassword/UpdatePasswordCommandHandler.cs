843cf9cea08fe7dae53b621748e49d1b0a217c77
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-13 17:30:43 +0300

    update password commands added

---
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/UpdatePassword/UpdatePasswordCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using ETicaretAPI.Application.Exceptions;
+using MediatR;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.UpdatePassword
+{
+    public class UpdatePasswordCommandHandler : IRequestHandler<UpdatePasswordCommandRequest, UpdatePasswordCommandResponse>
+    {
+        readonly IUserService userService;
+
+        public UpdatePasswordCommandHandler(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public async Task<UpdatePasswordCommandResponse> Handle(UpdatePasswordCommandRequest request, CancellationToken cancellationToken)
+        {
+            if (!request.NewPassword.Equals(request.NewPasswordConfirm))
+            {
+                throw new PasswordChangeFailedException();
+            }
+            await userService.UpdatePasswordAsync(request.UserId, request.ResetToken, request.NewPassword);
+            return new();
+        }
+    }
+}
---
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/UpdatePassword/UpdatePasswordCommandRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.UpdatePassword
+{
+    public class UpdatePasswordCommandRequest:IRequest<UpdatePasswordCommandResponse>
+    {
+        public string UserId { get; set; }
+        public string ResetToken { get; set; }
+        public string NewPassword { get; set; }
+        public string NewPasswordConfirm { get; set; }
+    }
+}
