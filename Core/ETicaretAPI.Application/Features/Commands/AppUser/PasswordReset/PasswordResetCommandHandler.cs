37e6254d00b2607c86931ae5517d0b3cef5283a3
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-13 12:34:33 +0300

    PasswordReset command classes added

---
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/PasswordReset/PasswordResetCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.PasswordReset
+{
+    public class PasswordResetCommandHandler : IRequestHandler<PasswordResetCommandRequest, PasswordResetCommandResponse>
+    {
+        readonly IAuthService authService;
+
+        public PasswordResetCommandHandler(IAuthService authService)
+        {
+            this.authService = authService;
+        }
+
+        public async Task<PasswordResetCommandResponse> Handle(PasswordResetCommandRequest request, CancellationToken cancellationToken)
+        {
+            await authService.PasswordResetAsync(request.Email);
+            return new();
+        }
+    }
+}
