6c5cef951b02596b3fc15423155d1324492396b9
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-14 20:35:27 +0300

    Completed order commands added

---
+++ Core/ETicaretAPI.Application/Features/Commands/Order/CompleteOrder/CompleteOrderCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using MediatR;
+
+namespace ETicaretAPI.Application.Features.Commands.Order.CompleteOrder
+{
+    public class CompleteOrderCommandHandler : IRequestHandler<CompleteOrderCommandRequest, CompleteOrderCommandResponse>
+    {
+        readonly IOrderService orderService;
+
+        public CompleteOrderCommandHandler(IOrderService orderService)
+        {
+            this.orderService = orderService;
+        }
+
+        public Task<CompleteOrderCommandResponse> Handle(CompleteOrderCommandRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/ETicaretAPI.Application/Features/Commands/Order/CompleteOrder/CompleteOrderCommandRequest.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace ETicaretAPI.Application.Features.Commands.Order.CompleteOrder
+{
+    public class CompleteOrderCommandRequest:IRequest<CompleteOrderCommandResponse>
+    {
+    }
+}
