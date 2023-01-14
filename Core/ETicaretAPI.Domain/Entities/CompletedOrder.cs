04071dbf79a4f1f84f65226a459f783118ffcdfc
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-14 20:40:40 +0300

    Completed order entity column type updated and mig_22 added

--- Core/ETicaretAPI.Domain/Entities/CompletedOrder.cs
+++ Core/ETicaretAPI.Domain/Entities/CompletedOrder.cs
@@ -4,7 +4,7 @@ namespace ETicaretAPI.Domain.Entities
 {
     public class CompletedOrder : BaseEntity
     {
-        public int OrderId { get; set; }
+        public Guid OrderId { get; set; }
         public Order Order{ get; set; }
     }
 }
--- Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
+++ Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
@@ -20,11 +20,13 @@ namespace ETicaretAPI.Persistence.Contexts
         public DbSet<InvoiceFile> InvoiceFiles { get; set; }
         public DbSet<Basket> Baskets { get; set; }
         public DbSet<BasketItem> BasketItems { get; set; }
+        public DbSet<CompletedOrder> CompletedOrders { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Order>().HasKey(x => x.ID);
-            builder.Entity<Basket>().HasOne(x => x.Order).WithOne(x=>x.Basket).HasForeignKey<Order>(x=>x.ID);
+            builder.Entity<Basket>().HasOne(x => x.Order).WithOne(x => x.Basket).HasForeignKey<Order>(x => x.ID);
             builder.Entity<Order>().HasIndex(x => x.OrderCode).IsUnique();
+            builder.Entity<Order>().HasOne(x => x.CompletedOrder).WithOne(x => x.Order).HasForeignKey<CompletedOrder>(x => x.OrderId);
             base.OnModelCreating(builder);
         }
         public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
