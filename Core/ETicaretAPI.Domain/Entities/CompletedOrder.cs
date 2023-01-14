a5f0086d024dedf86e0b3d5ea80a40a078a6b751
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-14 20:34:35 +0300

    CompletedOrder entity and completed order read and write concrete classes are added

---
+++ Core/ETicaretAPI.Domain/Entities/CompletedOrder.cs
@@ -0,0 +1,10 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+
+namespace ETicaretAPI.Domain.Entities
+{
+    public class CompletedOrder : BaseEntity
+    {
+        public int OrderId { get; set; }
+        public Order Order{ get; set; }
+    }
+}
--- Core/ETicaretAPI.Domain/Entities/Order.cs
+++ Core/ETicaretAPI.Domain/Entities/Order.cs
@@ -4,12 +4,10 @@ namespace ETicaretAPI.Domain.Entities
 {
     public class Order : BaseEntity
     {
-        //public Guid CustomerId { get; set; }
         public string Description { get; set; }
         public string Address { get; set; }
-        //public Customer Customer { get; set; }
         public string OrderCode { get; set; }
         public Basket Basket { get; set; }
-        //public ICollection<Product> Products { get; set; }
+        public CompletedOrder CompletedOrder { get; set; }
     }
 }
