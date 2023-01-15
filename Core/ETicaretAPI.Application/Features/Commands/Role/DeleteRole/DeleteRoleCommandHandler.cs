2cd92af45d9ba0c463abb7241b94e3613197d961
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-15 20:15:02 +0300

    Delete role commands added

---
+++ Core/ETicaretAPI.Application/Features/Commands/Role/DeleteRole/DeleteRoleCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.Role.DeleteRole
+{
+    public class DeleteRoleCommandHandler : IRequestHandler<DeleteRoleCommandRequest, DeleteRoleCommandResponse>
+    {
+        readonly IRoleService roleService;
+
+        public DeleteRoleCommandHandler(IRoleService roleService)
+        {
+            this.roleService = roleService;
+        }
+
+        public async Task<DeleteRoleCommandResponse> Handle(DeleteRoleCommandRequest request, CancellationToken cancellationToken)
+        {
+            var result = await roleService.DeleteRole(request.Name);
+            return new()
+            {
+                Succeeded = result
+            };
+        }
+    }
+}
