5455844b43cd4ed3e2e87b0f36873e0f971054fa
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-15 12:26:08 +0300

    GetAuthorizeDefinitionEndpoints method updated

--- Infrastructure/ETicaretAPI.Infrastructure/Services/Configurations/ApplicationService.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/Configurations/ApplicationService.cs
@@ -15,46 +15,48 @@ namespace ETicaretAPI.Infrastructure.Services.Configurations
         public List<Menu> GetAuthorizeDefinitionEndpoints(Type type)
         {
             Assembly assembly = Assembly.GetAssembly(type);
-            var controllers = assembly.GetTypes().Where(x => x.IsAssignableTo(typeof(ControllerBase)));
+            var controllers = assembly.GetTypes().Where(t => t.IsAssignableTo(typeof(ControllerBase)));
+
             List<Menu> menus = new();
             if (controllers != null)
-            {
                 foreach (var controller in controllers)
                 {
-                    var actions = controller.GetMethods().Where(x => x.IsDefined(typeof(AuthorizeDefinitionAttribute)));
+                    var actions = controller.GetMethods().Where(m => m.IsDefined(typeof(AuthorizeDefinitionAttribute)));
                     if (actions != null)
-                    {
                         foreach (var action in actions)
                         {
-                            var attributes = action.GetCustomAttributes();
+                            var attributes = action.GetCustomAttributes(true);
                             if (attributes != null)
                             {
                                 Menu menu = null;
-                                var authorizeDefinitionAttribute = attributes.FirstOrDefault(x => x.GetType() == typeof(AuthorizeDefinitionAttribute)) as AuthorizeDefinitionAttribute;
-                                if (menus.Any(x => x.Name == authorizeDefinitionAttribute.Menu))
+
+                                var authorizeDefinitionAttribute = attributes.FirstOrDefault(a => a.GetType() == typeof(AuthorizeDefinitionAttribute)) as AuthorizeDefinitionAttribute;
+                                if (!menus.Any(m => m.Name == authorizeDefinitionAttribute.Menu))
                                 {
                                     menu = new() { Name = authorizeDefinitionAttribute.Menu };
                                     menus.Add(menu);
                                 }
                                 else
-                                    menu = menus.FirstOrDefault(x => x.Name == authorizeDefinitionAttribute.Menu);
-                                Action _action = new()
+                                    menu = menus.FirstOrDefault(m => m.Name == authorizeDefinitionAttribute.Menu);
+
+                                Application.DTOs.Configuration.Action _action = new()
                                 {
-                                    ActionType = Enum.GetName(typeof(ActionType),authorizeDefinitionAttribute.ActionType),
+                                    ActionType = Enum.GetName(typeof(ActionType), authorizeDefinitionAttribute.ActionType),
                                     Definition = authorizeDefinitionAttribute.Definition
                                 };
-                                var httpAttribute = attributes.FirstOrDefault(x => x.GetType().IsAssignableTo(typeof(HttpMethodAttribute))) as HttpMethodAttribute;
+
+                                var httpAttribute = attributes.FirstOrDefault(a => a.GetType().IsAssignableTo(typeof(HttpMethodAttribute))) as HttpMethodAttribute;
                                 if (httpAttribute != null)
                                     _action.HttpType = httpAttribute.HttpMethods.First();
                                 else
                                     _action.HttpType = HttpMethods.Get;
+
                                 _action.Code = $"{_action.HttpType}.{_action.ActionType}.{_action.Definition.Replace(" ", "")}";
+
                                 menu.Actions.Add(_action);
                             }
                         }
-                    }
                 }
-            }
             return menus;
         }
     }
