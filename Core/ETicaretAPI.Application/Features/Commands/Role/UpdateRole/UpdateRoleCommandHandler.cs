3d2b97e873dcad34c83f0f5acfc4f7ba14cee4fb
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-15 20:15:11 +0300

    Update role commands added

---
+++ Core/ETicaretAPI.Application/Features/Commands/Role/UpdateRole/UpdateRoleCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.Role.UpdateRole
+{
+    public class UpdateRoleCommandHandler : IRequestHandler<UpdateRoleCommandRequest, UpdateRoleCommandResponse>
+    {
+        readonly IRoleService roleService;
+
+        public UpdateRoleCommandHandler(IRoleService roleService)
+        {
+            this.roleService = roleService;
+        }
+
+        public async Task<UpdateRoleCommandResponse> Handle(UpdateRoleCommandRequest request, CancellationToken cancellationToken)
+        {
+            var result = await roleService.UpdateRole(request.Id, request.Name);
+            return new()
+            {
+                Succeeded = result
+            };
+        }
+    }
+}
