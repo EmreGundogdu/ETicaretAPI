55cdfccfab3ce91d0aadcc886fd1bab0f6661c0f
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-15 11:53:45 +0300

    Dependencies added to IoC

--- Infrastructure/ETicaretAPI.Infrastructure/ServiceRegistration.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/ServiceRegistration.cs
@@ -1,6 +1,10 @@
-﻿using ETicaretAPI.Application.Abstractions.Storage;
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using ETicaretAPI.Application.Abstractions.Services.Configurations;
+using ETicaretAPI.Application.Abstractions.Storage;
 using ETicaretAPI.Application.Abstractions.Token;
 using ETicaretAPI.Infrastructure.Enums;
+using ETicaretAPI.Infrastructure.Services;
+using ETicaretAPI.Infrastructure.Services.Configurations;
 using ETicaretAPI.Infrastructure.Services.Storage;
 using ETicaretAPI.Infrastructure.Services.Storage.Azure;
 using ETicaretAPI.Infrastructure.Services.Storage.Local;
@@ -15,6 +19,8 @@ namespace ETicaretAPI.Infrastructure
         {
             services.AddScoped<IStorageService, StorageService>();
             services.AddScoped<ITokenHandler, TokenHandler>();
+            services.AddScoped<IMailService, MailService>();
+            services.AddScoped<IApplicationService, ApplicationService>();
         }
         public static void AddStorage<T>(this IServiceCollection services) where T : Storage, IStorage
         {
