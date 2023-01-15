98dae94b2ba5395bb0057551874a9665dc2f0d1f
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-15 20:14:51 +0300

    Create role commands added

---
+++ Core/ETicaretAPI.Application/Features/Commands/Role/CreateRole/CreateRoleCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.Role.CreateRole
+{
+    public class CreateRoleCommandHandler : IRequestHandler<CreateRoleCommandRequest, CreateRoleCommandResponse>
+    {
+        readonly IRoleService roleService;
+
+        public CreateRoleCommandHandler(IRoleService roleService)
+        {
+            this.roleService = roleService;
+        }
+
+        public async Task<CreateRoleCommandResponse> Handle(CreateRoleCommandRequest request, CancellationToken cancellationToken)
+        {
+            var result = await roleService.CreateRole(request.Name);
+            return new()
+            {
+                Succeeded = result
+            };
+        }
+    }
+}
---
+++ Core/ETicaretAPI.Application/Features/Commands/Role/CreateRole/CreateRoleCommandRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace ETicaretAPI.Application.Features.Commands.Role.CreateRole
+{
+    public class CreateRoleCommandRequest:IRequest<CreateRoleCommandResponse>
+    {
+        public string Name { get; set; }
+    }
+
+}
