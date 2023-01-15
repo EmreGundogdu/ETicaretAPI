48b7191e9c2b8521a1d90bb62712d430fc194f1a
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-15 20:15:34 +0300

    Get roles queries added

---
+++ Core/ETicaretAPI.Application/Features/Queries/Role/GetRoles/GetRolesQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Queries.Role.GetRoles
+{
+    public class GetRolesQueryHandler : IRequestHandler<GetRolesQueryRequest, GetRolesQueryResponse>
+    {
+        readonly IRoleService roleService;
+
+        public GetRolesQueryHandler(IRoleService roleService)
+        {
+            this.roleService = roleService;
+        }
+
+        public async Task<GetRolesQueryResponse> Handle(GetRolesQueryRequest request, CancellationToken cancellationToken)
+        {
+            var result =  roleService.GetAllRoles();
+            return new()
+            {
+                Datas = result
+            };
+        }
+    }
+}
