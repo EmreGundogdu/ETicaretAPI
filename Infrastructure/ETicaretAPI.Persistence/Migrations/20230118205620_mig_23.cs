906673c45d881b4c95faf87066348ceb7ad4144e
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-18 23:56:44 +0300

    mig 23 added

---
+++ Infrastructure/ETicaretAPI.Persistence/Migrations/20230118205620_mig_23.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ETicaretAPI.Persistence.Migrations
+{
+    public partial class mig_23 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Menus",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menus", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Endpoints",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    MenuID = table.Column<Guid>(type: "uuid", nullable: false),
+                    ActionType = table.Column<string>(type: "text", nullable: false),
+                    HttpType = table.Column<string>(type: "text", nullable: false),
+                    Definition = table.Column<string>(type: "text", nullable: false),
+                    Code = table.Column<string>(type: "text", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Endpoints", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Endpoints_Menus_MenuID",
+                        column: x => x.MenuID,
+                        principalTable: "Menus",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppRoleEndpoint",
+                columns: table => new
+                {
+                    AppRolesId = table.Column<string>(type: "text", nullable: false),
+                    EndpointsID = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppRoleEndpoint", x => new { x.AppRolesId, x.EndpointsID });
+                    table.ForeignKey(
+                        name: "FK_AppRoleEndpoint_AspNetRoles_AppRolesId",
+                        column: x => x.AppRolesId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AppRoleEndpoint_Endpoints_EndpointsID",
+                        column: x => x.EndpointsID,
+                        principalTable: "Endpoints",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppRoleEndpoint_EndpointsID",
+                table: "AppRoleEndpoint",
+                column: "EndpointsID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Endpoints_MenuID",
+                table: "Endpoints",
+                column: "MenuID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppRoleEndpoint");
+
+            migrationBuilder.DropTable(
+                name: "Endpoints");
+
+            migrationBuilder.DropTable(
+                name: "Menus");
+        }
+    }
+}
