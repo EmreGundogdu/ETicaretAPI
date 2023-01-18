2bb0026aec3e3377efc039367cba44677eac4351
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-18 23:54:52 +0300

    Endpoint menu entities added

---
+++ Core/ETicaretAPI.Domain/Entities/Endpoint.cs
@@ -0,0 +1,20 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using ETicaretAPI.Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Domain.Entities
+{
+    public class Endpoint:BaseEntity
+    {
+        public Menu Menu { get; set; }
+        public string ActionType { get; set; }
+        public string HttpType { get; set; }
+        public string Definition { get; set; }
+        public string Code { get; set; }
+        public ICollection<AppRole> AppRoles{ get; set; }
+    }
+}
--- Core/ETicaretAPI.Domain/Entities/Identity/AppRole.cs
+++ Core/ETicaretAPI.Domain/Entities/Identity/AppRole.cs
@@ -4,6 +4,6 @@ namespace ETicaretAPI.Domain.Entities.Identity
 {
     public class AppRole : IdentityRole<string>
     {
-
+        public ICollection<Endpoint> Endpoints { get; set; }
     }
 }
