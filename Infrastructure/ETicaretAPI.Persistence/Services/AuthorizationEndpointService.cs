c744d42eac1654ddb93795487d001e4f27a138e9
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-19 00:08:11 +0300

    GetAuthorizeDefinitionEndpoints method parameter updated

--- Infrastructure/ETicaretAPI.Persistence/Services/AuthorizationEndpointService.cs
+++ Infrastructure/ETicaretAPI.Persistence/Services/AuthorizationEndpointService.cs
@@ -1,12 +1,29 @@
 ﻿using ETicaretAPI.Application.Abstractions.Services;
+using ETicaretAPI.Application.Abstractions.Services.Configurations;
+using ETicaretAPI.Application.Repositories;
 
 namespace ETicaretAPI.Persistence.Services
 {
     public class AuthorizationEndpointService : IAuthorizationEndpointService
     {
-        public Task AssignRoleEndpointAsync(string[] roles, string code)
+        readonly IApplicationService applicationService;
+        readonly IEndpointReadRepository endpointReadRepository;
+        readonly IEndpointWriteRepository endpointWriteRepository;
+
+        public AuthorizationEndpointService(IApplicationService applicationService, IEndpointReadRepository endpointReadRepository, IEndpointWriteRepository endpointWriteRepository)
         {
-            throw new NotImplementedException();
+            this.applicationService = applicationService;
+            this.endpointReadRepository = endpointReadRepository;
+            this.endpointWriteRepository = endpointWriteRepository;
+        }
+
+        public async Task AssignRoleEndpointAsync(string[] roles, string code,Type type)
+        {
+            var endpoint = await endpointReadRepository.GetSingleAsync(x => x.Code == code);
+            if (endpoint == null)
+            {
+                applicationService.GetAuthorizeDefinitionEndpoints(type);
+            }
         }
     }
 }
