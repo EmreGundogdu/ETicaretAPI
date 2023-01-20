72030dfe4472e932b53388233d470a581a5d7edb
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-20 18:55:15 +0300

    get all users query request parameter updated

--- Core/ETicaretAPI.Application/Features/Queries/User/GetAllUsers/GetAllUsersQueryRequest.cs
+++ Core/ETicaretAPI.Application/Features/Queries/User/GetAllUsers/GetAllUsersQueryRequest.cs
@@ -5,6 +5,6 @@ namespace ETicaretAPI.Application.Features.Queries.User.GetAllUsers
     public class GetAllUsersQueryRequest:IRequest<GetAllUsersQueryResponse>
     {
         public int Page { get; set; } = 0;
-        public int PageSize { get; set; } = 0;
+        public int PageSize { get; set; } = 5;
     }
 }
