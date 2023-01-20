9a47f3d0e2ef20fe14d6e772f8df7e551a9d69a2
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-20 18:55:26 +0300

    get roles to user queries updated

---
+++ Core/ETicaretAPI.Application/Features/Queries/User/GetRolesToUser/GetRolesToUserQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Queries.User.GetRolesToUser
+{
+    public class GetRolesToUserQueryHandler : IRequestHandler<GetRolesToUserQueryRequest, GetRolesToUserQueryResponse>
+    {
+        readonly IUserService userService;
+
+        public GetRolesToUserQueryHandler(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public async Task<GetRolesToUserQueryResponse> Handle(GetRolesToUserQueryRequest request, CancellationToken cancellationToken)
+        {
+            var response = await userService.GetRolesToUserAsync(request.UserId);
+            return new()
+            {
+                UserRoles = response
+            };
+        }
+    }
+}
---
+++ Core/ETicaretAPI.Application/Features/Queries/User/GetRolesToUser/GetRolesToUserQueryResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace ETicaretAPI.Application.Features.Queries.User.GetRolesToUser
+{
+    public class GetRolesToUserQueryResponse
+    {
+        public string[] UserRoles { get; set; }
+    }
+}
