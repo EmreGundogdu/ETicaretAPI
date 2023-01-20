892b417abde68ecf05b5bfbb54a88d888454c2f3
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-20 21:59:07 +0300

    Qr code service abstract class and concrete classes added with GenerateQRCode method

---
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/QRCodeService.cs
@@ -0,0 +1,22 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Infrastructure.Services
+{
+    public class QRCodeService : IQRCodeService
+    {
+        public byte[] GenerateQRCode(string text)
+        {
+            QRCodeGenerator qRCodeGenerator = new();
+            QRCodeData data = qRCodeGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            PngByteQRCode qrCode = new(data);
+            byte[] qrCodeGraphic = qrCode.GetGraphic(10, new byte[] { 84, 99, 71 }, new byte[] { 240, 240, 240 });
+            return qrCodeGraphic;
+        }
+    }
+}
