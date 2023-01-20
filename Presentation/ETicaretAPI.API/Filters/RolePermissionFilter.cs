f1cd5ad4b82e102ff320244d4efabd13a23dd9b4
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-20 21:33:09 +0300

    RolePermissionFilter added

---
+++ Presentation/ETicaretAPI.API/Filters/RolePermissionFilter.cs
@@ -0,0 +1,45 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using ETicaretAPI.Application.CustomAttributes;
+using Google.Apis.Util;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System.Reflection;
+
+namespace ETicaretAPI.API.Filters
+{
+    public class RolePermissionFilter : IAsyncActionFilter
+    {
+        readonly IUserService userService;
+
+        public RolePermissionFilter(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var name = context.HttpContext.User.Identity?.Name;
+            if (!string.IsNullOrEmpty(name))
+            {
+                var descriptor = context.ActionDescriptor as ControllerActionDescriptor;
+                var attribute = descriptor.MethodInfo.GetCustomAttribute(typeof(AuthorizeDefinitionAttribute)) as AuthorizeDefinitionAttribute;
+                var httpAttribute = descriptor.MethodInfo.GetCustomAttribute(typeof(HttpMethodAttribute)) as HttpMethodAttribute;
+
+                var code = $"{(httpAttribute != null ? httpAttribute.HttpMethods.First() : HttpMethods.Get)}.{attribute.ActionType}.{attribute.Definition.Replace(" ", "")}";
+                var hasRole = await userService.HasRolePermissionToEndpointAsync(name,code);
+                if (!hasRole)
+                {
+                    context.Result = new UnauthorizedResult();
+                }
+                else
+                {
+                    await next();
+                }
+            }
+            await next();
+
+        }
+    }
+}
