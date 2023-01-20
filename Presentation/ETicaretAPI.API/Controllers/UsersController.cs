10ccc1acb40aeaa7bfd9c715fc2131bde43b46ae
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-20 12:52:31 +0300

    get all users endpoint added to users controller

--- Presentation/ETicaretAPI.API/Controllers/UsersController.cs
+++ Presentation/ETicaretAPI.API/Controllers/UsersController.cs
@@ -1,6 +1,9 @@
-﻿using ETicaretAPI.Application.Features.Commands.AppUser.CreateUser;
+﻿using ETicaretAPI.Application.CustomAttributes;
+using ETicaretAPI.Application.Features.Commands.AppUser.CreateUser;
 using ETicaretAPI.Application.Features.Commands.AppUser.UpdatePassword;
+using ETicaretAPI.Application.Features.Queries.User.GetAllUsers;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ETicaretAPI.API.Controllers
@@ -24,9 +27,18 @@ namespace ETicaretAPI.API.Controllers
         }
 
         [HttpPost("update-password")]
-        public async Task<IActionResult> UpdatePassword([FromBody]UpdatePasswordCommandRequest updatePasswordCommandRequest)
+        public async Task<IActionResult> UpdatePassword([FromBody] UpdatePasswordCommandRequest updatePasswordCommandRequest)
         {
             var response = await mediator.Send(updatePasswordCommandRequest);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = "Admin")]
+        [AuthorizeDefinition(ActionType = Application.Enums.ActionType.Reading, Definition = "Get All Users", Menu = "Users")]
+        public async Task<IActionResult> GetAllUsers([FromQuery]GetAllUsersQueryRequest getAllUsersQueryRequest)
+        {
+            var response = await mediator.Send(getAllUsersQueryRequest);
             return Ok(response);
         }
     }
