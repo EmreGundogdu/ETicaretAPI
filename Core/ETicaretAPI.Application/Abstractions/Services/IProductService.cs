c1b7b991fc3b708350bdf997907a98e7dd0d8d3b
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-20 21:59:27 +0300

    GenerateQRCode method used in product service abstraction and in concrete class

---
+++ Core/ETicaretAPI.Application/Abstractions/Services/IProductService.cs
@@ -0,0 +1,7 @@
+﻿namespace ETicaretAPI.Application.Abstractions.Services
+{
+    public interface IProductService
+    {
+        Task<byte[]> QrCodeToProductAsync(string productId);
+    }
+}
---
+++ Infrastructure/ETicaretAPI.Persistence/Services/ProductService.cs
@@ -0,0 +1,41 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using ETicaretAPI.Application.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Services
+{
+    public class ProductService : IProductService
+    {
+        readonly IProductReadRepository productReadRepository;
+        readonly IQRCodeService qRCodeService;
+        public ProductService(IProductReadRepository productReadRepository, IQRCodeService qRCodeService)
+        {
+            this.productReadRepository = productReadRepository;
+            this.qRCodeService = qRCodeService;
+        }
+
+        public async Task<byte[]> QrCodeToProductAsync(string productId)
+        {
+            var product = await productReadRepository.GetByIdAsync(productId);
+            if (product is null)
+            {
+                throw new Exception("Not found");
+            }
+            var plainObject = new
+            {
+                product.ID,
+                product.Name,
+                product.Price,
+                product.Stock,
+                product.CreatedDate
+            };
+            string platinText = JsonSerializer.Serialize(plainObject);
+            return qRCodeService.GenerateQRCode(platinText);
+        }
+    }
+}
