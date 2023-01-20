be8e1d56f0600d962bc7c5b04f87a626620ae53b
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-20 18:55:02 +0300

    assign role to user commands added

---
+++ Core/ETicaretAPI.Application/Features/Commands/User/AssignRoleToUser/AssignRoleToUserCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using MediatR;
+
+namespace ETicaretAPI.Application.Features.Commands.User.AssignRoleToUser
+{
+    public class AssignRoleToUserCommandHandler : IRequestHandler<AssignRoleToUserCommandRequest, AssignRoleToUserCommandResponse>
+    {
+        readonly IUserService userService;
+
+        public AssignRoleToUserCommandHandler(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public async Task<AssignRoleToUserCommandResponse> Handle(AssignRoleToUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            await userService.AssignRoleToUserAsync(request.UserId, request.Roles);
+            return new();
+        }
+    }
+}
